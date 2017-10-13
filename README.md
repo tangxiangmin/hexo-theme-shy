9989c302427f397a6700a331a4c5b525a27de387
tangxiangmin <645234650@qq.com>
2017-10-13 23:56:23 +0800

    post & archives

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Shy
 ===
 > 一个Hexo主题
+
+## 文档
+* 遍历标签需要使用`post.tags.forEach(function(item){})`函数
---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by admin on 2017/10/13.
+ */
+
+
+$("#J_toggleSide").on("click", function(){
+    console.log(1);
+})
