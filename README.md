2f7914c9762796901b4271bdb37dd6793e699849
tangxiangmin <645234650@qq.com>
2017-10-15 00:50:18 +0800

    tags

--- README.md
+++ README.md
@@ -2,5 +2,34 @@ Shy
 ===
 > 一个Hexo主题
 
-## 文档
-* 遍历标签需要使用`post.tags.forEach(function(item){})`函数
+## 遇见的坑
+
+### 新增自定义页面
+首先
+```
+hexo new page test
+```
+然后发现在`source`目录下生成了对应的`test/index.md`，修改其`Front-matter`的`layout`属性，指定对应模板
+```
+---
+title: test
+date: 2017-10-14 22:38:14
+layout: "test"
+comments: false
+---
+```
+
+然后在对应的主题`layout`目录新增模板即可，这里指定的模板名称是`layout.ejs`
+
+### 常见变量的属性
+文档里面只给出了全局变量和页面变量，但是对于单个变量比如`tag`并没有很详细的说明（也许是我没看见），所以采用最原始的`console`获取对应变量的属性，并记录在下面
+
+* tag
+    * name，标签名
+    * length，标签的数量，可以用来制作标签云
+* category，跟标签基本类似
+    * name，分类名
+    * length，分类的数量
+
+
+
--- source/js/main.js
+++ source/js/main.js
@@ -2,7 +2,75 @@
  * Created by admin on 2017/10/13.
  */
 
+$(function(){
+    var Aside = {
+        $switchBtn: $("#J_toggleSide"),
+        init: function () {
+            var $btn = this.$switchBtn,
+                $aside = $(".page_sd"),
+                $main = $("#blog");
 
-$("#J_toggleSide").on("click", function(){
-    console.log(1);
-})
+            $btn.on("click", function(){
+                $aside.toggleClass("active");
+                $main.toggleClass("active");
+
+                $btn.toggleClass("close");
+            });
+
+            $btn.on("mouseover", function () {
+                $btn.addClass("hover");
+            }).on("mouseout", function () {
+                $btn.removeClass("hover");
+            })
+        },
+
+        show: function(){
+
+        },
+        hide: function(){}
+    };
+
+    var BackTop = {
+        speed: 2000,
+        $btn: $(".btn-top"),
+        $body: $(document.body),
+        setVisible: function(){
+            var $btn = this.$btn;
+            this.$body.scrollTop() > 300 ?  $btn.addClass("active") : $btn.removeClass("active");
+        },
+
+        listen: function(){
+            var $btn = this.$btn,
+                $body = this.$body;
+
+            this.setVisible();
+            var self = this;
+
+            $(document).on("scroll", function () {
+                self.setVisible();
+            });
+
+            // todo debounce
+            $btn.on("click", function () {
+                $body.animate({
+                    scrollTop: 0,
+                    speed: this.speed
+                })
+            }.bind(this))
+        }
+    };
+
+    var App = {
+        init: function(){
+            BackTop.listen();
+            Aside.init();
+        }
+    };
+
+    App.init();
+
+
+});
+
+
+
